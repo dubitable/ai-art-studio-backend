6e40ff254f5dd466ec462570f703151543a87459
dubitable <pierrequereuil@gmail.com>
2022-12-31 15:28:40 -0800

    Version Fixes

--- app/views.py
+++ app/views.py
@@ -10,35 +10,37 @@ from django.views.decorators.csrf import csrf_exempt
 import re
 from django.shortcuts import render
 
+
 def index(request, template="index.html", ):
     return render(request, template, {})
 
+
 @csrf_exempt
 def sdk_gen(request):
-  body = json.loads(request.body)
-  image_data = re.sub('^data:image/.+;base64,', '', body['start_img'])
-  im = Image.open(BytesIO(base64.b64decode(image_data)))
-  im = im.resize((768, 512))
-  stability_api = client.StabilityInference(
-    key= "sk-LSGz4ZgKzzNrgvTe4K4yz5XbPHCJdCnulW9xZNVGS0n2k8q7", 
-    verbose=True,
-  )
+    body = json.loads(request.body)
+    image_data = re.sub('^data:image/.+;base64,', '', body['start_img'])
+    im = Image.open(BytesIO(base64.b64decode(image_data)))
+    im = im.resize((768, 512))
+    stability_api = client.StabilityInference(
+        key="sk-KdPQlYdDb3dw22xawhlZvWX37fV6sCCb7Oo803WbsQgO3Wri",
+        verbose=True,
+    )
 
-  answers = stability_api.generate(
-    prompt= body["input"],
-    seed= "", # if provided, specifying a random seed makes results deterministic
-    steps=100, # defaults to 50 if not specified
-    init_image= im
-  )
+    answers = stability_api.generate(
+        prompt=body["input"],
+        seed="",  # if provided, specifying a random seed makes results deterministic
+        steps=100,  # defaults to 50 if not specified
+        init_image=im
+    )
 
 # iterating over the generator produces the api response
-  for resp in answers:
-      for artifact in resp.artifacts:
-          if artifact.finish_reason == generation.FILTER:
-              return warnings.warn(
+    for resp in answers:
+        for artifact in resp.artifacts:
+            if artifact.finish_reason == generation.FILTER:
+                return warnings.warn(
                     "Your request activated the API's safety filters and could not be processed."
                     "Please modify the prompt and try again.")
-          if artifact.type == generation.ARTIFACT_IMAGE:
-              store = artifact.binary
-              img = str(base64.b64encode(store))
-              return JsonResponse({'img': img[2:-1]})
+            if artifact.type == generation.ARTIFACT_IMAGE:
+                store = artifact.binary
+                img = str(base64.b64encode(store))
+                return JsonResponse({'img': img[2:-1]})
